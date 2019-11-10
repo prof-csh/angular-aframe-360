8fa424bdf52476d44c1ede2cb9089658ced15814
prof-csh <prof.csh@gmail.com>
2019-11-10 02:23:20 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-cjeweu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-cjeweu)
---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'wec-root',
+  template: '<wec-scene></wec-scene>',
+  encapsulation: ViewEncapsulation.None,
+})
+export class AppComponent {
+}
